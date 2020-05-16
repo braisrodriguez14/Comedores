656c573db34bc164a9798658bc393b24c9837b2e
braisrodriguez14 <braisrodriguez14@gmail.com>
2020-05-16 18:03:29 +0200

    Añadir primer caso de prueba

--- PrjGrupo3C/test/guardardatos/GuardarMenu.java
+++ PrjGrupo3C/test/guardardatos/GuardarMenu.java
@@ -2,13 +2,67 @@ package guardardatos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import Implementacion.DAOMenuImp;
+import Interfaces.DAOMenu;
+import Interfaces.Menu;
 
 class GuardarMenu {
 
+	private DAOMenu daoMenu;
+	
+	@BeforeAll
+	static void preparacionClase() throws IOException {
+		//Se comprueba si existe el fichero
+		File archivo = new File("Menus.txt");
+
+		if (archivo.createNewFile()) {
+		    System.out.println("Se ha creado el archivo Menus.txt");
+		} 
+		else {
+		    System.out.println("El archivo Menus.txt ya existe");
+		}
+	}
+	
+	@BeforeEach
+	void preparacionCasoPrueba(){
+		//Se crea un nuevo objeto DAOMenu antes de ejecutar un nuevo caso de prueba
+		daoMenu = new DAOMenuImp();
+		
+		//Se elimina el contenido del fichero Estadisticas.txt
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("Menus.txt", false));
+			out.write("");
+			out.close();
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("UC_0022_P_01_CP_01")
+	void UC_0022_P_01_CP_01() {
+		//Arrange
+		Menu menu = null;
+						
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+				
+		//Assert 1 -> Se comprueba que el menu no se haya guardado en el archivo
+		assertEquals(0, archivo.length());
 	}
 
 }
