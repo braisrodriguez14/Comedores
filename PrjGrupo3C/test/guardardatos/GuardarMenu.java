9f668c9adf1a96dc9963e8fcf00eeaad221bea7d
braisrodriguez14 <braisrodriguez14@gmail.com>
2020-05-17 13:40:40 +0200

    Pruebas caja negra guardar menu

--- PrjGrupo3C/test/guardardatos/GuardarMenu.java
+++ PrjGrupo3C/test/guardardatos/GuardarMenu.java
@@ -6,13 +6,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import Implementacion.DAOMenuImp;
+import Implementacion.MenuImp;
+import Implementacion.Plato;
 import Interfaces.DAOMenu;
 import Interfaces.Menu;
 
@@ -53,7 +57,7 @@ class GuardarMenu {
 	
 	
 	@Test
-	@DisplayName("UC_0022_P_01_CP_01")
+	@DisplayName("Guardar menu P_01 CP_01")
 	void UC_0022_P_01_CP_01() {
 		//Arrange
 		Menu menu = null;
@@ -63,8 +67,144 @@ class GuardarMenu {
 		
 		File archivo = new File("Menus.txt");
 				
-		//Assert 1 -> Se comprueba que el menu no se haya guardado en el archivo
+		//Assert
 		assertEquals(0, archivo.length());
+		
+		/*
+		 * Se comprueba que el menu no se haya guardado en el archivo ya que no es un menu valido
+		 */
+	}
+	
+	@Test
+	@DisplayName("Guardar menu P_01 CP_02")
+	void UC_0022_P_01_CP_02() {
+		//Arrange
+		Menu menu = new MenuImp();
+		
+		for(int i = 0; i < 3; i ++) {
+			menu.anhadirEntrante(new Plato());
+			menu.anhadirPrincipal(new Plato());
+			menu.anhadirPostre(new Plato());
+		}
+		
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+				
+		//Assert
+		assertEquals(0, archivo.length());
+		
+		/*
+		 * Se comprueba que el menu no se haya guardado en el archivo ya que no es un menu valido
+		 */
+	}
+	
+	@Test
+	@DisplayName("Guardar menu P_01 CP_03")
+	void UC_0022_P_01_CP_03() {
+		//Arrange
+		Menu menu = new MenuImp();
+		menu.setFecha(LocalDate.now());
+		
+		for(int i = 0; i < 3; i ++) {
+			menu.anhadirPrincipal(new Plato());
+			menu.anhadirPostre(new Plato());
+		}
+		
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+				
+		//Assert
+		assertEquals(0, archivo.length());
+		
+		/*
+		 * Se comprueba que el menu no se haya guardado en el archivo ya que no es un menu valido
+		 */
+	}
+	
+	@Test
+	@DisplayName("Guardar menu P_01 CP_04")
+	void UC_0022_P_01_CP_04() {
+		//Arrange
+		Menu menu = new MenuImp();
+		menu.setFecha(LocalDate.now());
+		
+		for(int i = 0; i < 3; i ++) {
+			menu.anhadirEntrante(new Plato());
+			menu.anhadirPostre(new Plato());
+		}
+		
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+				
+		//Assert
+		assertEquals(0, archivo.length());
+		
+		/*
+		 * Se comprueba que el menu no se haya guardado en el archivo ya que no es un menu valido
+		 */
+	}
+	
+	@Test
+	@DisplayName("Guardar menu P_01 CP_05")
+	void UC_0022_P_01_CP_05() {
+		//Arrange
+		Menu menu = new MenuImp();
+		menu.setFecha(LocalDate.now());
+		
+		for(int i = 0; i < 3; i ++) {
+			menu.anhadirEntrante(new Plato());
+			menu.anhadirPrincipal(new Plato());
+		}
+		
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+				
+		//Assert
+		assertEquals(0, archivo.length());
+		
+		/*
+		 * Se comprueba que el menu no se haya guardado en el archivo ya que no es un menu valido
+		 */
+	}
+	
+	@Test
+	@DisplayName("Guardar menu P_01 CP_06")
+	void UC_0022_P_01_CP_06() {
+		//Arrange
+		Menu menu = new MenuImp();
+		menu.setFecha(LocalDate.now());
+		
+		for(int i = 0; i < 3; i ++) {
+			menu.anhadirEntrante(new Plato("Ensalada", 50, "Entrante"));
+			menu.anhadirPrincipal(new Plato("Macarrones", 150, "Principal"));
+			menu.anhadirPostre(new Plato("Natillas", 100, "Postre"));
+		}
+		
+		//Act
+		daoMenu.guardarMenu(menu);
+		
+		File archivo = new File("Menus.txt");
+		
+		Menu menuGuardado = daoMenu.devolverMenus().get(0);
+		//Assert
+		Assertions.assertAll(
+				( ) -> assertNotNull(menuGuardado.getFecha()),
+				( ) -> assertEquals(3, menuGuardado.getEntrantes().size()),
+				( ) -> assertEquals(3, menuGuardado.getPrincipales().size()),
+				( ) -> assertEquals(3, menuGuardado.getPostres().size())
+		);
+		
+		/*
+		 * Se comprueba que el menu se ha guardado correctamente en el archivo
+		 */
 	}
 
 }
